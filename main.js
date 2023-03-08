2f1f59d00bcf6589360fb450119ca20896d0fe69
Matheus Ferretti <125493830+MatheusFerretti-1508@users.noreply.github.com>
2023-03-08 06:40:16 -0300

    Correção

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('form').on('submit', function(e) {
+    e.preventDefault();
+
+    const tarefaNova = $('<li></li>');
+    const nomeTarefa = $('#nome-tarefa').val();
+
+    console.log(nomeTarefa)
+    $("<li>").html(`${nomeTarefa}`).appendTo('ul');
+
+    $('#nome-tarefa').val(" ")
+
+    $('li').click(function(){
+        $(this).css("text-decoration","line-through");
+        })
+
+    })
+
+
+
+
+
+
+
+
+
+})
