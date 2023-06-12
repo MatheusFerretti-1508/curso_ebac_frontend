fc7449866e3af4b25e12a9b20b98a3a1c6a7d5ac
Matheus Ferretti <125493830+MatheusFerretti-1508@users.noreply.github.com>
2023-06-12 19:34:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (valor1, valor2) => valor1 * valor2;
+console.log(multiplicacao(6, 8));
+const boasVindas = (nome, segundoNome) => `${nome} diz olá para ${segundoNome}`;
+console.log(boasVindas('Matheus', 'Juliana'));
---
+++ main.ts
@@ -0,0 +1,5 @@
+const multiplicacao = (valor1: number, valor2: number): number => valor1 * valor2;
+console.log(multiplicacao(6,8));
+
+const boasVindas = (nome: string, segundoNome: string) => `${nome} diz olá para ${segundoNome}`;
+console.log(boasVindas('Matheus', 'Juliana'))
